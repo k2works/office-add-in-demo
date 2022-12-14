139d7293f59a52939e86a0cc439ba82c0b36354c
k2works <kakimomokkuri@gmail.com>
2022-12-14 14:19:41 +0900

    update for production

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ const CopyWebpackPlugin = require("copy-webpack-plugin");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 const urlDev = "https://localhost:3000/";
-const urlProd = "https://www.contoso.com/"; // CHANGE THIS TO YOUR PRODUCTION DEPLOYMENT LOCATION
+const urlProd = "https://office-add-in-demo-gqwk.vercel.app/";
 
 async function getHttpsOptions() {
   const httpsOptions = await devCerts.getHttpsServerOptions();
